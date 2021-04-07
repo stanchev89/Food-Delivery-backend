f30ff332d67e2fff9a30fbd695ff08c2232fadc5
Stefan Stanchev <62604562+stanchev89@users.noreply.github.com>
2021-04-07 22:59:04 +0300

    Update config.js

--- config/config.js
+++ config/config.js
@@ -4,12 +4,12 @@ const config = {
 	development: {
 		port: process.env.PORT || 3500,
 		dbURL: process.env.DB_URL,
-		origin: [ "http://localhost:3000", "http://localhost:4200", "http://localhost:3001" ]
+		origin: [ "http://localhost:3000", "http://localhost:4200", "http://localhost:3001","https://food-delivery-stanchev.herokuapp.com" ]
 	},
 	production: {
 		port: process.env.PORT,
 		dbURL: process.env.DB_URL,
-		origin: [ "http://localhost:3000", "http://localhost:4200", "http://localhost:3001" ]
+		origin: [ "https://food-delivery-stanchev.herokuapp.com" ]
 	}
 };
 
