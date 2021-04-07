2bf7ae731cae0f389fc933e57abf767ba543f267
Stefan Stanchev <stanchev89@gmail.com>
2021-04-07 21:45:54 +0300

    fix

--- config/config.js
+++ config/config.js
@@ -8,7 +8,7 @@ const config = {
 	},
 	production: {
 		port: process.env.PORT,
-		dbURL: process.env.DB_URL_CREDENTIALS,
+		dbURL: process.env.DB_URL_CREDENTIALS || "mongodb+srv://stanchev89:stanchev89@cluster0.qyezc.gcp.mongodb.net/delivery?retryWrites=true&w=majority",
 		origin: [ "http://localhost:3000", "http://localhost:4200", "http://localhost:3001" ]
 	}
 };
