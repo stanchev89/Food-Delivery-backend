6a1567967037ff0fb708300dd7a6ef037a729da6
Stefan Stanchev <stanchev89@gmail.com>
2021-04-07 21:52:43 +0300

    delete mongo url

--- config/config.js
+++ config/config.js
@@ -3,12 +3,12 @@ const env = process.env.NODE_ENV || "development";
 const config = {
 	development: {
 		port: process.env.PORT || 3500,
-		dbURL: process.env.DB_URL_CREDENTIALS || "mongodb+srv://stanchev89:stanchev89@cluster0.qyezc.gcp.mongodb.net/delivery?retryWrites=true&w=majority",
+		dbURL: process.env.DB_URL,
 		origin: [ "http://localhost:3000", "http://localhost:4200", "http://localhost:3001" ]
 	},
 	production: {
 		port: process.env.PORT,
-		dbURL: process.env.DB_URL_CREDENTIALS || "mongodb+srv://stanchev89:stanchev89@cluster0.qyezc.gcp.mongodb.net/delivery?retryWrites=true&w=majority",
+		dbURL: process.env.DB_URL,
 		origin: [ "http://localhost:3000", "http://localhost:4200", "http://localhost:3001" ]
 	}
 };
