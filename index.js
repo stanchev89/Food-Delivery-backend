fe0e48bf91ac267dd5f06429a4f8f15dc4dfce3d
Stefan Stanchev <62604562+stanchev89@users.noreply.github.com>
2021-04-08 11:01:33 +0300

    add middleware

--- index.js
+++ index.js
@@ -16,6 +16,11 @@ dbConnector()
 		require("./config/express")(app);
 
 		app.use(cors());
+		app.use(function(req, res, next) {
+			res.header("Access-Control-Allow-Origin", "*");
+			res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+			next();
+		  });
 
 		app.use(apiRouter);
 
