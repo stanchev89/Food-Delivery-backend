33f31e4472af9dd56654ea08778b94970501288f
Stefan Stanchev <62604562+stanchev89@users.noreply.github.com>
2021-04-08 11:35:03 +0300

    remove custom function for cors
    remove custom function for cors

--- index.js
+++ index.js
@@ -16,11 +16,6 @@ dbConnector()
 		require("./config/express")(app);
 
 		app.use(cors());
-		app.use(function(req, res, next) {
-			res.header("Access-Control-Allow-Origin", "*");
-			res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-			next();
-		  });
 
 		app.use(apiRouter);
 
